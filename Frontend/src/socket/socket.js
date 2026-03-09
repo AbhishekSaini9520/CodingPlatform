79c0eecd4240b715252aa081907589f8f7d70f6d
Abhishek Saini <abhisheksaini200404@gmail.com>
2026-03-09 21:50:28 +0530

    update the socket

--- Frontend/src/socket/socket.js
+++ Frontend/src/socket/socket.js
@@ -1,8 +1,9 @@
 import { io } from "socket.io-client";
+const BASE_URL = "https://codingplatform-3ipe.onrender.com";
 
-const URL = "http://localhost:4000";
+// const URL = "http://localhost:4000";
 
-export const socket = io(URL, {
+export const socket = io(BASE_URL, {
   autoConnect: true,
   withCredentials: true
 });
